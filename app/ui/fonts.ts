50b2985bd1740956fbe376b20cdf3dfaaa802004
Christian Milner <christianmilner@Christians-Mac-mini.local>
2024-12-26 14:43:48 +0000

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google'
+import { Figtree } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const figtree = Figtree({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400'] })
