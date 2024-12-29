d8b2c0df1b60877a2b7ae85cf75438d8337ec9dd
Christian Milner <118346815+christianmilner@users.noreply.github.com>
2024-12-29 15:55:06 +0000

    updated invoice forms

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,21 @@
 
 import { annotateDynamicAccess } from 'next/dist/server/app-render/dynamic-rendering';
 import { z } from 'zod';
+import { createClient } from '@supabase/supabase-js'
+import { config } from 'dotenv';
+
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+ 
+// Load environment variables from .env file
+config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or Key');
+}
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const FormSchema = z.object({
     id: z.string(),
@@ -12,6 +27,7 @@ const FormSchema = z.object({
   });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status } = CreateInvoice.parse({ 
@@ -24,5 +40,55 @@ export async function createInvoice(formData: FormData) {
     const date = new Date().toISOString().split('T')[0];
 
 
-console.log('Creating invoice with:', { customerId, amount, status });
+    console.log('Creating invoice with:', { customerId, amount, status });
+
+    const { error } = await supabase
+    .from('invoices')
+    .insert({ customer_id: customerId, amount: amountInCents, status, date });
+
+  if (error) {
+    console.error('Error inserting invoice:', error);
+    throw new Error('Failed to create invoice');
+  }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+
+  const { error } = await supabase
+  .from('invoices')
+  .update({ customer_id: customerId, amount: amountInCents, status})
+  .eq('id', id);
+
+if (error) {
+  console.error('Error updating invoice:', error);
+  throw new Error('Failed to update invoice');
+}
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  const { error } = await supabase
+  .from('invoices')
+  .delete()
+  .eq('id', id);
+
+  if (error) {
+    console.error('Error updating invoice:', error);
+    throw new Error('Failed to update invoice');
+  }
+
+  revalidatePath('/dashboard/invoices');
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -143,41 +143,6 @@ export async function fetchLatestInvoices() {
 //   }
 // }
 
-// const ITEMS_PER_PAGE = 6;
-// export async function fetchFilteredInvoices(
-//   query: string,
-//   currentPage: number,
-// ) {
-//   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
-//   try {
-//     const invoices = await sql<InvoicesTable>`
-//       SELECT
-//         invoices.id,
-//         invoices.amount,
-//         invoices.date,
-//         invoices.status,
-//         customers.name,
-//         customers.email,
-//         customers.image_url
-//       FROM invoices
-//       JOIN customers ON invoices.customer_id = customers.id
-//       WHERE
-//         customers.name ILIKE ${`%${query}%`} OR
-//         customers.email ILIKE ${`%${query}%`} OR
-//         invoices.amount::text ILIKE ${`%${query}%`} OR
-//         invoices.date::text ILIKE ${`%${query}%`} OR
-//         invoices.status ILIKE ${`%${query}%`}
-//       ORDER BY invoices.date DESC
-//       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-//     `;
-
-//     return invoices.rows;
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch invoices.');
-//   }
-// }
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
@@ -220,27 +185,6 @@ export async function fetchFilteredInvoices(
   }
 }
 
-// export async function fetchInvoicesPages(query: string) {
-//   try {
-//     const count = await sql`SELECT COUNT(*)
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE
-//       customers.name ILIKE ${`%${query}%`} OR
-//       customers.email ILIKE ${`%${query}%`} OR
-//       invoices.amount::text ILIKE ${`%${query}%`} OR
-//       invoices.date::text ILIKE ${`%${query}%`} OR
-//       invoices.status ILIKE ${`%${query}%`}
-//   `;
-
-//     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-//     return totalPages;
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch total number of invoices.');
-//   }
-// }
-
 export async function fetchInvoicesPages(query: string) {
   try {
     const { data, error, count } = await supabase
@@ -271,49 +215,29 @@ export async function fetchInvoicesPages(query: string) {
   }
 }
 
-// export async function fetchInvoiceById(id: string) {
-//   try {
-//     const data = await sql<InvoiceForm>`
-//       SELECT
-//         invoices.id,
-//         invoices.customer_id,
-//         invoices.amount,
-//         invoices.status
-//       FROM invoices
-//       WHERE invoices.id = ${id};
-//     `;
+export async function fetchInvoiceById(id: string) {
+  try {
+    const { data, error } = await supabase
+      .from('invoices')
+      .select('*')
+      .eq('id', id);
 
-//     const invoice = data.rows.map((invoice) => ({
-//       ...invoice,
-//       // Convert amount from cents to dollars
-//       amount: invoice.amount / 100,
-//     }));
+    const invoice = Array.isArray(data) ? data.map((invoice) => ({
+        ...invoice,
+        // Convert amount from cents to dollars
+        amount: invoice.amount / 100,
+      })) : [];
+      
+      if (!Array.isArray(data)) {
+        console.error('Expected data to be an array, but got:', data);
+      }
+    return invoice[0];
 
-//     return invoice[0];
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch invoice.');
-//   }
-// }
-
-// export async function fetchCustomers() {
-//   try {
-//     const data = await sql<CustomerField>`
-//       SELECT
-//         id,
-//         name
-//       FROM customers
-//       ORDER BY name ASC
-//     `;
-
-//     const customers = data.rows;
-//     return customers;
-//   } catch (err) {
-//     console.error('Database Error:', err);
-//     throw new Error('Failed to fetch all customers.');
-//   }
-// }
-
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoice.');
+  }
+}
 export async function fetchCustomers() {
   try {
     const { data , error } = await supabase
